5d8196e4b259c5d54f7a0fa2f655304570220b5d
ayhugo <hayre@oucs1391.otago.ac.nz>
2017-05-24 12:07:42 +1200

    Basic GUI

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var standards: UITextField!
+    @IBOutlet weak var howMany: UITextField!
+    @IBOutlet weak var price: UITextField!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calc(_ sender: UIButton) {
+        let priceFloat = Float(price.text!)!
+        let howManyFloat = Float(howMany.text!)!
+        let standardsFloat = Float(standards.text!)!
+        
+        let test = numStandards(priceFloat, howManyFloat, standardsFloat)
+        label.text = String(describing: test)
+    }
 
 }
 
---
+++ test/test-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  test-Bridging-Header.h
+//  test
+//
+//  Created by Hugo Ayre on 5/24/17.
+//  Copyright © 2017 Team Alpha. All rights reserved.
+//
+
+#include "header.h"
